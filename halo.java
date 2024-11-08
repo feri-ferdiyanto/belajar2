36ed1710d9d01a5bcc921b97d1c0c693dfda1966
abdillah-hash <abdi.lllah126@gmail.com>
2024-11-08 11:23:08 +0700

    add changes for conditions

--- halo.java
+++ halo.java
@@ -3,6 +3,12 @@ public class halo {
       
         System.out.println("Hello, new");
         System.out.println("Hallo");
+
+        int a = 2;
+        if(a => 1){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
     }
-    
 }
